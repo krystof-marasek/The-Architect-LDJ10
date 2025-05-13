77833efe5ecc06b6acf6b487da52ac399223b20f
Krystof Marasek <krystofmarasek@gmail.com>
2025-05-13 21:35:42 +0200

    Door animation created

---
+++ The Architect/Assets/Scripts/Buttons/ButtonInteractable.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+
+public class ButtonInteractable : MonoBehaviour, IInteractable
+{
+    [SerializeField] private MeshRenderer sphereMeshRenderer;
+    [SerializeField] private Material blueMaterial;
+    [SerializeField] private Material yellowMaterial;
+
+    private bool isColorYellow;
+
+    private void SetColorBlue()
+    {
+        sphereMeshRenderer.material = blueMaterial;
+    }
+
+    private void SetColorYellow()
+    {
+        sphereMeshRenderer.material = yellowMaterial;
+    }
+
+    private void ToggleColor()
+    {
+        isColorYellow = !isColorYellow;
+        if (isColorYellow)
+        {
+            SetColorYellow();
+        }
+        else
+        {
+            SetColorBlue();
+        }
+    }
+
+    public void PushButton()
+    {
+        ToggleColor();
+    }
+
+    public void Interact(Transform interactorTransform)
+    {
+        PushButton();
+    }
+
+    public string GetInteractText()
+    {
+        return "Push button";
+    }
+
+    public Transform GetTransform()
+    {
+        return transform;
+    }
+}
---
+++ The Architect/Assets/Scripts/Doors/DoorInteractable.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class DoorInteractable : MonoBehaviour, IInteractable
+{
+    private Animator animator;
+    private bool isOpen;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void ToggleDoor()
+    {
+        isOpen = !isOpen;
+        animator.SetBool("IsOpen", isOpen);
+    }
+
+    public void Interact(Transform interactorTransform)
+    {
+        ToggleDoor();
+    }
+
+    public string GetInteractText()
+    {
+        return "Open/Close Door";
+    }
+
+    public Transform GetTransform()
+    {
+        return transform;
+    }
+}
